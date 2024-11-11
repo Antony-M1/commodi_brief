27aa94e5a526889f65e3b3a6a1647e0dde780e9e
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-11-11 16:47:12 +0530

    Main app update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import google.generativeai as genai
+import os
 import streamlit as st
-from cb_util import get_logger
+from cb_util import get_logger, get_hash, VectorDB, get_summary
 
 
 logger_st = get_logger("Streamlit", "app.log")
 
 
 logger_st.info("App Started Running")
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 st.title("Document Summarizer")
 
@@ -16,5 +21,12 @@ uploaded_file = st.file_uploader("Upload PDF", type="pdf")
 if uploaded_file:
     logger_st.info("File uploaded successfully")
     with st.spinner("Extracting and processing PDF..."):
-        import time
-        time.sleep(5)
+        hash_value = get_hash(uploaded_file)
+        vectordb = VectorDB(uploaded_file=uploaded_file)
+        st.info("Data loaded successfully")
+        query = st.text_input("Ask your Query")
+        summary_btn = st.button("Get Summary", type='primary')
+        if summary_btn:
+            knn = vectordb.get_knn(query)
+            response = get_summary(model, knn)
+            st.write(response)
